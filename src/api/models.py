a3628f6da31257d2603a05064a88e59e335d9b40
mhavliczek <32149001+mhavliczek@users.noreply.github.com>
2024-11-16 03:30:06 +0000

    optimizacion de codigo

--- src/api/models.py
+++ src/api/models.py
@@ -5,11 +5,11 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    nombre = db.Column(db.String(50), nullable=True)
-    apellido = db.Column(db.String(50), nullable=True)
-    ciudad = db.Column(db.String(100), nullable=True)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    nombre = db.Column(db.String(50))
+    apellido = db.Column(db.String(50))
+    ciudad = db.Column(db.String(100))
+    is_active = db.Column(db.Boolean, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -21,5 +21,5 @@ class User(db.Model):
             "nombre": self.nombre,
             "apellido": self.apellido,
             "ciudad": self.ciudad,
-            # do not serialize the password, its a security breach
+            # Password is excluded for security reasons
         }
--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, jsonify, url_for, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
@@ -7,145 +7,125 @@ from api.models import db, User
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
-from flask_cors import CORS  # Se importa CORS
-from flask_jwt_extended import jwt_required
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from flask_bcrypt import Bcrypt
-from flask_jwt_extended import create_access_token
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import JWTManager
 
-# Configuración del entorno
-ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
+# Configuración del entorno y parámetros
+ENV = os.getenv("FLASK_ENV", "development")
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-# Setup de JWT
+# Configuración del JWT
 app.config["JWT_SECRET_KEY"] = "super-secret"
 jwt = JWTManager(app)
 
 # Configuración de la base de datos
-db_url = os.getenv("DATABASE_URL")
-if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
-
+db_url = os.getenv("DATABASE_URL", "sqlite:////tmp/test.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://") if db_url else db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Inicializar dependencias
 MIGRATE = Migrate(app, db, compare_type=True)
 db.init_app(app)
 bcrypt = Bcrypt(app)
 
-# Habilitar CORS globalmente para todas las rutas
+# Habilitar CORS globalmente
 CORS(app)
 
-# Configurar el Admin
+# Configurar Admin y Comandos
 setup_admin(app)
-
-# Configurar comandos
 setup_commands(app)
 
-# Registrar todas las rutas con el prefijo "api"
+# Registrar rutas con prefijo 'api'
 app.register_blueprint(api, url_prefix='/api')
 
-# Manejar errores de manera adecuada
+# Manejar errores
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# Generar mapa de las rutas
+# Generar mapa de rutas
 @app.route('/')
 def sitemap():
-    if ENV == "development":
-        return generate_sitemap(app)
-    return send_from_directory(static_file_dir, 'index.html')
+    return generate_sitemap(app) if ENV == "development" else send_from_directory(static_file_dir, 'index.html')
 
 # Servir archivos estáticos
 @app.route('/<path:path>', methods=['GET'])
-def serve_any_other_file(path):
+def serve_static_files(path):
     if not os.path.isfile(os.path.join(static_file_dir, path)):
         path = 'index.html'
     response = send_from_directory(static_file_dir, path)
-    response.cache_control.max_age = 0  # Evitar cache
+    response.cache_control.max_age = 0  # Desactivar cache
     return response
 
-# Registro de Usuario
+# Registro de usuario
 @app.route("/signup", methods=["POST"])
 def signup():
     data = request.get_json()
-    app.logger.info("Dato Recibido del registro: %s", data)
-    email = data.get("email")
-    password = data.get("password")
-    nombre = data.get("nombre")
-    apellido = data.get("apellido")
-    ciudad = data.get("ciudad")
-
-    usuario_existe = User.query.filter_by(email=email).first()
-    if usuario_existe:
+    app.logger.info("Registro recibido: %s", data)
+    
+    # Verificación de existencia de usuario
+    user = User.query.filter_by(email=data.get("email")).first()
+    if user:
         return jsonify({"msg": "Email ya existe"}), 400
     
+    # Crear nuevo usuario
     nuevo_usuario = User(
-        email=email,
-        nombre=nombre,
-        apellido=apellido,
-        ciudad=ciudad,
+        email=data.get("email"),
+        nombre=data.get("nombre"),
+        apellido=data.get("apellido"),
+        ciudad=data.get("ciudad"),
         is_active=True,
-        password=bcrypt.generate_password_hash(password).decode('utf-8'),
+        password=bcrypt.generate_password_hash(data.get("password")).decode('utf-8'),
     )
     db.session.add(nuevo_usuario)
     db.session.commit()
+    
+    # Crear token de acceso
     access_token = create_access_token(identity=nuevo_usuario.id)
     return jsonify({"token": access_token, "user_id": nuevo_usuario.id}), 201
 
-# Login de Usuario
+# Login de usuario
 @app.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
-    email = data.get("email")
-    password = data.get("password")
-    user = User.query.filter_by(email=email).first()
+    user = User.query.filter_by(email=data.get("email")).first()
 
-    if user and bcrypt.check_password_hash(user.password, password):
+    # Verificación de credenciales
+    if user and bcrypt.check_password_hash(user.password, data.get("password")):
         access_token = create_access_token(identity=user.id)
         return jsonify({"token": access_token, "user_id": user.id}), 200
-    else:
-        return jsonify({"msg": "Usuario erróneo o mal password."}), 401
+    return jsonify({"msg": "Usuario o contraseña incorrectos."}), 401
 
-# Cerrar Sesión
+# Cerrar sesión
 @app.route("/logout", methods=["POST"])
 @jwt_required()
 def logout():
     return jsonify({"msg": "Sesión cerrada exitosamente"}), 200
 
-# Usuario Privado
+# Endpoint privado
 @app.route("/private/<int:user_id>", methods=["GET"])
 @jwt_required()
 def private(user_id):
     current_user_id = get_jwt_identity()
-
-    if current_user_id == user_id:
-        user = User.query.get(user_id)
-        if user:
-            response_data = {
-                "id": user.id,
-                "email": user.email,
-                "nombre": user.nombre,
-                "apellido": user.apellido,
-                "ciudad": user.ciudad,
-                "is_active": user.is_active,
-            }
-            return jsonify(response_data), 200
-        else:
-            return jsonify({"message": "Usuario no encontrado"}), 404
-    else:
+    
+    if current_user_id != user_id:
         return jsonify({"message": "Acceso no autorizado"}), 403
+    
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"message": "Usuario no encontrado"}), 404
+    
+    response_data = {key: getattr(user, key) for key in ['id', 'email', 'nombre', 'apellido', 'ciudad', 'is_active']}
+    return jsonify(response_data), 200
 
 # Endpoint protegido
 @app.route("/protected", methods=["GET"])
 @jwt_required()
 def protected():
-    usuario_actual = get_jwt_identity()
-    return jsonify(logged_in_as=usuario_actual), 200
+    return jsonify(logged_in_as=get_jwt_identity()), 200
 
 # Iniciar la aplicación
 if __name__ == '__main__':
