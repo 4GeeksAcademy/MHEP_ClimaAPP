e0ca2e39a49c65724d653814d2e3c3c31760b025
mhavliczek <32149001+mhavliczek@users.noreply.github.com>
2024-12-09 05:13:27 +0000

    cambios en login

--- src/front/js/component/Inicio.js
+++ src/front/js/component/Inicio.js
@@ -1,9 +1,25 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
+import { auth, provider } from "./firebase"; // Asegúrate de que la ruta sea correcta
+import { signInWithPopup } from "firebase/auth";
 import "../../styles/inicio.css";
 
 const Inicio = () => {
+  const handleGoogleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      console.log("Usuario autenticado:", user);
+      // Aquí puedes manejar el usuario autenticado, como guardar el token en el localStorage
+      localStorage.setItem("jwt-token", user.accessToken);
+      // Redirigir o actualizar el estado de la aplicación según sea necesario
+      window.location.href = `/private/${user.uid}`; // Redirige al perfil del usuario
+    } catch (error) {
+      console.error("Error al iniciar sesión con Google:", error);
+    }
+  };
+  
   return (
     <div className="fondo-inicio">
       <Container className="contenedor-inicio">
@@ -32,5 +48,5 @@ const Inicio = () => {
     </div>
   );
 };
-
+console.log("Inicio componente Inicio");
 export default Inicio;
---
+++ src/front/js/component/firebase.js
@@ -0,0 +1,20 @@
+// firebase.js
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyApjxCBvwLVsW8B6WFLsgJ3AxCoMEM8--I",
+  authDomain: "app-de-clima-cd5ef.firebaseapp.com",
+  projectId: "app-de-clima-cd5ef",
+  storageBucket: "app-de-clima-cd5ef.appspot.com",
+  messagingSenderId: "157882657628",
+  appId: "1:157882657628:web:3a8df06718e466630eef99",
+  measurementId: "G-TRHDBC0V03",
+};
+
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider };
